720c4728d15458fe0901c4bac924afaf4cfba96b
EmadDeve20 <emaddeve20@gmail.com>
2025-11-12 23:49:08 +0330

    update settings parameters

--- diana/settings.py
+++ diana/settings.py
@@ -10,6 +10,15 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 PROXY_URL = os.getenv("PROXY_URL") or None
 
+HUGGINGFACE_KEY = os.getenv("HUGGINGFACE_KEY")
+
+MODEL = os.getenv("MODEL")
+
+TEMPERATURE = float(os.getenv("TEMPERATURE") or "0")
+
+SYSTEM_PROMPT = os.getenv("SYSTEM_PROMPT")
+
+OWNER_USERNAME = os.getenv("OWNER_USERNAME")
 
 logger = logging.getLogger(__name__)
 
