ee0a3ada0220e2a1776fe80cfd56ee2b74b337be
EmadDeve20 <emaddeve20@gmail.com>
2025-11-12 23:51:00 +0330

    update chat message handler and connect it to AI Agent

--- diana/bot.py
+++ diana/bot.py
@@ -1,12 +1,16 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
+from langchain_core.messages.human import HumanMessage
+
 from diana.settings import (
     TELEGRAM_TOKEN,
     PROXY_URL,
+    OWNER_USERNAME,
     logger
 )
 
+from diana.agent import agent
 
 
 # TODO: Complte help
@@ -15,7 +19,14 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"you sent: {update.message.text}")
+    if update.effective_user.username == OWNER_USERNAME:
+        user_message = HumanMessage(update.message.text)
+        reply_message = await agent.ainvoke({"messages": user_message})
+        await update.message.reply_text(reply_message["messages"][-1].content)
+    
+    # TODO: Handle if it is not owner
+    else: 
+        ...
 
 
 def run_bot():
