0e826cc064a43603e83aaf519d726df3ab8c4bc9
EmadDeve20 <emaddeve20@gmail.com>
2025-11-13 20:23:54 +0330

    update agent to use memory

--- diana/agent.py
+++ diana/agent.py
@@ -1,11 +1,15 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 from langchain.agents.factory import create_agent
+from langchain_core.messages.human import HumanMessage
+from langgraph.graph import MessagesState
+from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
 
 from diana.settings import (
     HUGGINGFACE_KEY,
     TEMPERATURE,
     MODEL,
-    SYSTEM_PROMPT
+    SYSTEM_PROMPT,
+    AI_MEMORY_DB
 )
 
 from diana.ai_tools import TOOLS
@@ -18,11 +22,33 @@ chat_endpoint = HuggingFaceEndpoint(
 
 chat_model = ChatHuggingFace(llm=chat_endpoint)
 
-# TODO: Check this is ok or not? maybe we need to use react agent
-# TODO: Add Memeory
-agent = create_agent(
-    model=chat_model,
-    tools=TOOLS,
-    system_prompt=SYSTEM_PROMPT
-)
 
+# TODO: research for best practice to use agent with memory
+# maybe it is better to make our agent befor like builder.
+# I mean befor compile, and then compile our graph in this function 
+async def run_agent(thread_id:int, human_message:str) -> MessagesState:
+    """
+    talk with your agent or execute agent
+
+    Args:
+        thread_id (int): thread id
+        human_message (str): message of human
+
+    Returns:
+        MessagesState: return MessagesState
+    """
+
+    config = {"configurable": {"thread_id": thread_id}}
+
+    async with AsyncSqliteSaver.from_conn_string(AI_MEMORY_DB) as checkpointer:
+
+        agent = create_agent(
+            model=chat_model,
+            tools=TOOLS,
+            system_prompt=SYSTEM_PROMPT,
+            checkpointer=checkpointer
+        )
+
+        res = await agent.ainvoke({"messages": HumanMessage(human_message)}, config=config)
+
+        return res
