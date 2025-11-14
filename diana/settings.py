792e7af9b94de8bc4165866baff6cd0c91f509e3
EmadDeve20 <emaddeve20@gmail.com>
2025-11-14 20:00:27 +0330

    update settings

--- diana/settings.py
+++ diana/settings.py
@@ -1,3 +1,5 @@
+# TODO: Change Settings and use pydantic_settings to keep settings parameters
+
 import os
 import logging
 
@@ -21,6 +23,12 @@ SYSTEM_PROMPT = os.getenv("SYSTEM_PROMPT")
 OWNER_USERNAME = os.getenv("OWNER_USERNAME")
 
 AI_MEMORY_DB = os.getenv("AI_MEMORY_DB") or "checkpoints.sqlite"
+
+DATABASE_URL = os.getenv("DATABASE_URL") or "postgresql+asyncpg://user:password@127.0.0.1:5432/diana"
+
+DEBUG:str = os.getenv("DEBUG") or "true"
+
+DEBUG:bool = True if DEBUG.lower() == "true" else False
 
 logger = logging.getLogger(__name__)
 
