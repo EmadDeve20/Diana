42a27a1b8c2917a6845f6b22e40f8c8cde1f45a0
EmadDeve20 <emaddeve20@gmail.com>
2025-11-14 20:01:57 +0330

    update run script to migrate Tables

--- run_bot.py
+++ run_bot.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+import asyncio
 
 from diana.bot import run_bot
+from diana.database import create_all_tables
 
-run_bot()
+
+def main() -> None:
+    asyncio.run(create_all_tables())
+    run_bot()
+
+
+if __name__ == "__main__":
+
+    main()
 
