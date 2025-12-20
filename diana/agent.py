fa834345ab01aa61c7f8cc64dccf5cf9f136a731
EmadDeve20 <emaddeve20@gmail.com>
2025-12-20 21:16:25 +0330

    add logging middlewares

--- diana/agent.py
+++ diana/agent.py
@@ -4,9 +4,15 @@ from langchain_core.messages.human import HumanMessage
 from langgraph.graph import MessagesState
 from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
 
-from diana.settings import settings
 
+
+from diana.settings import settings
+from diana.middlewares import (
+    log_request_middleware,
+    log_response_middleware,
+)
 from diana.ai_tools import TOOLS
+
 
 chat_endpoint = HuggingFaceEndpoint(
     model=settings.MODEL,
@@ -18,8 +24,8 @@ chat_model = ChatHuggingFace(llm=chat_endpoint)
 
 
 # TODO: research for best practice to use agent with memory
-# maybe it is better to make our agent befor like builder.
-# I mean befor compile, and then compile our graph in this function 
+# maybe it is better to make our agent before like builder.
+# I mean before compile, and then compile our graph in this function 
 async def run_agent(thread_id:int, human_message:str) -> MessagesState:
     """
     talk with your agent or execute agent
@@ -40,7 +46,9 @@ async def run_agent(thread_id:int, human_message:str) -> MessagesState:
             model=chat_model,
             tools=TOOLS,
             system_prompt=settings.SYSTEM_PROMPT,
-            checkpointer=checkpointer
+            checkpointer=checkpointer,
+            middleware=[log_request_middleware,
+                        log_response_middleware]
         )
 
         res = await agent.ainvoke({"messages": HumanMessage(human_message)}, config=config)
