e5f1ba0b2431aad784d3a8ee135a3cac543eccac
EmadDeve20 <emaddeve20@gmail.com>
2025-12-21 22:52:32 +0330

    update bot to use updated run_agent and fix todo for help

--- diana/bot.py
+++ diana/bot.py
@@ -1,8 +1,18 @@
 import logging
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters
+)
 
+from langchain_core.messages import (
+    HumanMessage,
+    SystemMessage,
+)
 
 from diana.settings import settings
 
@@ -10,18 +20,27 @@ from diana.settings import settings
 from diana.agent import run_agent
 
 
-# TODO: Complte help
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hi {update.effective_user.first_name}! 👋')
+    system_message = SystemMessage(
+        "The user wants to know about you and your abilities. introduce about yourself."
+        " this is should be like a help page. Use the language most commonly used by the user."
+    )
+
+    ai_response = await run_agent(thread_id=update.effective_user.id,
+                                  message=system_message) 
+
+    await update.message.reply_text(ai_response["messages"][-1].content)
 
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
 
     if update.effective_user.username == settings.OWNER_USERNAME:
+        
+        message = HumanMessage(update.message.text)
 
         agent_response = await run_agent(thread_id=update.effective_user.id,
-                                         human_message=update.message.text)
+                                         message=message)
  
         await update.message.reply_text(agent_response["messages"][-1].content)
     
